2ed9ea71fd17b13b7d975b77caefb5ebaf64533d
acodyjr <acody@me.com>
2016-02-19 00:30:02 +0000

    Commit temperature conversion functions for synthesizing tasks.

---
+++ task_01.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""This module converts Fahrenheit temperature to Celsius."""
+
+import decimal
+
+ABSOLUTE_DIFFERENCE = decimal.Decimal('273.15')
+
+
+def fahrenheit_to_celsius(degrees):
+    """A function to convert Fahrenheit to Celsius.
+
+    Args:
+        degrees (dec): Fahrenheit input to be converted to Celsius.
+
+    Returns:
+        dec: Arithmetically calculated Celsius temperature conversion.
+
+    Examples:
+
+        >>> fahrenheit_to_celsius(32)
+        Decimal('0')
+
+        >>> fahrenheit_to_celsius(100)
+        Decimal('100')
+    """
+    degrees = round(degrees, 2)
+    
+    return decimal.Decimal((degrees - 32) * 5) / 9
+
+
+def celsius_to_kelvin(degrees):
+    """A function to convert Celsius to Kelvin.
+
+    Args:
+        degrees (dec): Celsius input to be converted to Kelvin.
+
+    Returns:
+        dec: Arithmetically converted Celsius temperature from Kelvin.
+
+    Examples:
+
+        >>> celsius_to_kelvin(100)
+        Decimal('373.15')
+    """
+    degrees = round(degrees, 2)
+    
+    return decimal.Decimal(degrees) + ABSOLUTE_DIFFERENCE
+
+
+def fahrenheit_to_kelvin(degrees):
+    """A function to convert Fahrenheit temperature to Kelvin.
+
+    Args:
+        degrees (dec): Fahrenheit input to be converted to Kelvin.
+
+    Returns:
+        dec: Arithmetically converted Kelvin temperature from Fahrenheit.
+
+    Examples:
+
+        >>> fahrenheit_to_kelvin(212)
+        Decimal('373.15')
+    """
+    degrees = round(degrees, 2)
+    
+    return celsius_to_kelvin(fahrenheit_to_celsius(degrees))
