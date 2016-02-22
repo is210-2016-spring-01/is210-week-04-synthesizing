56817ced48632754f67bb9e69560d34a5cd153fd
Daniella Sapozhnikova <daniellasapo@gmail.com>
2016-02-22 09:45:39 +0000

    hopefully done?

--- task_01.py
+++ task_01.py
@@ -16,8 +16,8 @@ def fahrenheit_to_celsius(degrees):
     to a *decimal* representation of degrees Celsius
 
     1. Deduct 32 from ``degrees``, then multiply by 5, then divide by 9"""
-    celsius = decimal.Decimal((degrees -32) * 5)/9
-    return celsius
+    celsius = decimal.Decimal(((degrees -32) * 5)/9)
+    return decimal.Decimal(('{:.2f}'.format(celsius)))
 
 
 def celsius_to_kelvin(degrees):
@@ -30,7 +30,7 @@ def celsius_to_kelvin(degrees):
 
     1.  add ``ABSOLUTE_DIFFERENCE`` to degrees"""
     kelvin = decimal.Decimal(degrees + ABSOLUTE_DIFFERENCE)
-    return kelvin
+    return decimal.Decimal(('{:.2f}'.format(kelvin)))
 
 
 def fahrenheit_to_kelvin(degrees):
@@ -42,4 +42,4 @@ def fahrenheit_to_kelvin(degrees):
     Fahrenheit temperatures to Kelvin and return the result as a number."""
     f_to_c = fahrenheit_to_celsius(degrees)
     f_to_k = (f_to_c + ABSOLUTE_DIFFERENCE)
-    return f_to_k
+    return decimal.Decimal(('{:.2f}'.format(f_to_k)))
