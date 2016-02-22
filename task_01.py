2e62a20ad665096500ef097a74e129ed05321d7d
ct3080a <coleen.thomas@spsmail.cuny.edu>
2016-02-22 13:59:33 +0000

    all tasks

---
+++ task_01.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# -*- conding: utf-8 -*-
+"""Fahrenheit to Celsius Calculator"""
+
+def fahrenheit_to_celsuis(degrees):
+    """Converts fahrenheit into Celsius.
+
+    Args:
+        degrees
+
+    Returns:
+        
+
+    Examples:
+        >>> fahrenheit_to_celsius(212)
+        Decimal('100')
+    """
+
+    return degrees * 9/5 + 32
---
+++ task_02.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""Function to switch Celsius to Kelvin"""
+
+import task_01.py
+
+ABSOLUTE_DIFFERENCE = 273.15
+
+def celsius_to_kelvin(degrees):
+    """Converts Celsius to Kelvin.
+
+    Args:
+
+    Returns:
+
+    Examples:
+        >>> fahrenheit_to_celsius(212)
+        Decimal('100')
+
+    """
+
+return ABSOLUTE_DIFFERENCE + degrees
