50cc837d24755632d084c55864b28a4c28dab9b2
Meriam Setti <mery81@hotmail.com>
2016-02-23 04:15:29 +0000

    Modif.

--- task_01.py
+++ task_01.py
@@ -6,28 +6,37 @@
 import decimal
 
 import task_02
-
 def fahrenheit_to_celcius(degrees):
     """Converts temperature from fahrenheit to celcius.
 
     Args:
-        degrees (int): Argument to be arithmetically converted.
+        degrees (int): Argument to be converted.
 
     Returns:
         decimal: conversion result returned as decimal.
 
     Examples:
 
-        >>> def fahrenheit_to_celcius(212)
+        >>> fahrenheit_to_celcius(212)
         Decimal('100')
         
     """
     return (decimal.Decimal(degrees - 32) * 5) / 9
 
 def fahrenheit_to_kelvin(degrees):
-    """
+    """Converts temperatures from fahrenheit to kelvin.
 
+    Args:
+        degrees (int): Argument to be converted.
+
+    Returns:
+        decimal: conversion result returned as decimal.
+
+    Examples:
+
+        >>> fahrenheit_to_kelvin(212)
+        Decimal(373.15')
+        
     """
-    return decimal.Decimal(task_02.celcius_to_kelvin(fahrenheit_to_celcius))
-    
-    
+    CONVERT2 = decimal.Decimal((((degrees - 32) * 5) / 9) + task_02.ABSOLUTE_DIFFERENCE)
+    return round(CONVERT2, 2)
